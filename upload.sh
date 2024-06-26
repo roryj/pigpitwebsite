798ac0bcc6fccbcaf791db63cd9cdd807ce46509
Rory Jacob <roryjacob@gmail.com>
2024-06-26 12:30:02 -0700

    Update upload file

--- upload.sh
+++ upload.sh
@@ -2,10 +2,18 @@
 
 BUCKET="pigpit.family"
 
+# For some reason the html files are picked up properly by the aws s3 regex here :shrug:
+# so instead we use find to get all the html files
 echo "uploading html files to $BUCKET"
-aws s3 cp ./*.html "s3://$BUCKET" --profile roryj-home
+find -name "*.html" | xargs -I {} aws s3 cp {} "s3://$BUCKET"
+
+echo "uploading js files to $BUCKET"
+aws s3 cp ./*.js "s3://$BUCKET"
+
+echo "uploading json files to $BUCKET"
+aws s3 cp ./*.json "s3://$BUCKET"
 
 echo "uploading image files to $BUCKET"
-aws s3 cp ./*.png "s3://$BUCKET" --profile roryj-home
+aws s3 cp ./*.png "s3://$BUCKET"
 
 echo "DONE"
