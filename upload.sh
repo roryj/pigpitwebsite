0b283b9b4c80c9cebd902cc529d5a6ffaba5c328
Rory Jacob <roryjacob@gmail.com>
2024-06-28 12:10:50 -0700

    fix upload file and fix bubbles

--- upload.sh
+++ upload.sh
@@ -5,10 +5,10 @@ BUCKET="pigpit.family"
 # For some reason the html files are picked up properly by the aws s3 regex here :shrug:
 # so instead we use find to get all the html files
 echo "uploading html files to $BUCKET"
-find -name "*.html" | xargs -I {} aws s3 cp {} "s3://$BUCKET"
+find . -name "*.html" -type f -exec aws s3 cp {} "s3://$BUCKET" \;
 
 echo "uploading js files to $BUCKET"
-aws s3 cp ./*.js "s3://$BUCKET"
+find . -name "*.js" -type f -exec aws s3 cp {} "s3://$BUCKET" \;
 
 echo "uploading json files to $BUCKET"
 aws s3 cp ./*.json "s3://$BUCKET"
