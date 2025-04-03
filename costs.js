ae9692ee2ef4d797943dcab14043880b65b336ad
Rory Jacob <roryjacob@gmail.com>
2025-04-03 13:10:01 -0500

    Reformat files

--- costs.js
+++ costs.js
@@ -1,117 +1,121 @@
-
-(async function() {
+(async function () {
   const result = await fetch("./costs.json");
 
   const costs = await result.json();
   console.log(costs);
 
-  costs.forEach(yearlyCostBreakdown => {
-    const year = yearlyCostBreakdown["year"]; 
+  costs.forEach((yearlyCostBreakdown) => {
+    const year = yearlyCostBreakdown["year"];
     const expenditures = yearlyCostBreakdown["expenditures"];
 
     const byCategoryMapping = byCategory(expenditures);
 
     const yearlyTotalCosts = totalCosts(expenditures);
     const yearlyTotalRevenue = totalRevenue(expenditures);
-    const yearlyTotalOperatingExpense = (yearlyTotalCosts + yearlyTotalRevenue).toFixed(2);
+    const yearlyTotalOperatingExpense = (
+      yearlyTotalCosts + yearlyTotalRevenue
+    ).toFixed(2);
 
     const totalCostHtml = document.getElementById(`${year}-total-cost`);
     totalCostHtml.textContent = `Operating Cost: ($${yearlyTotalCosts})\r\n\r\nRevenue: $${-yearlyTotalRevenue}\r\n\r\nTotal: ($${yearlyTotalOperatingExpense})`;
 
-    const labelOrdering = Object.keys(byCategoryMapping); 
+    const labelOrdering = Object.keys(byCategoryMapping);
     console.log(`LABELS: ${labelOrdering}`);
 
     // with X labels, then we need each dataset to include an array containing the data for that label:
     // ex: [labelA, labelB, labelC]
     // with data only applying to labelB, this would mean we would need to write the dataset as
     // [0, <number needed here>, 0]
-    const datasets = Object.entries(byCategoryMapping).map((keyValue, index) => {
+    const datasets = Object.entries(byCategoryMapping)
+      .map((keyValue, index) => {
         const expenditures = keyValue[1];
 
         return expenditures.map((exp) => {
-            const baseArray = Array(labelOrdering.length).fill(0);
-            baseArray[index] = exp.cost.toFixed(2);
-            return {
-                data: baseArray,
-                label: exp.name
-            }
+          const baseArray = Array(labelOrdering.length).fill(0);
+          baseArray[index] = exp.cost.toFixed(2);
+          return {
+            data: baseArray,
+            label: exp.name,
+          };
         });
-    }).flat();
-    
+      })
+      .flat();
 
-    new Chart(
-        document.getElementById(`${year}-costs`),
-        {
-            type: 'bar',
-            data: {
-                labels: labelOrdering,
-                datasets: datasets
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                },
-                responsive: true,
-                scales: {
-                  x: {
-                    stacked: true,
-                  },
-                  y: {
-                    stacked: true
-                  }
-                }
-            }
-        }
-    );
+    new Chart(document.getElementById(`${year}-costs`), {
+      type: "bar",
+      data: {
+        labels: labelOrdering,
+        datasets: datasets,
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true,
+          },
+        },
+      },
+    });
 
     // show table
     const tableBody = document.getElementById(`${year}-cost-table`);
-    expenditures.forEach(item => {
-        const row = document.createElement('tr');
+    expenditures.forEach((item) => {
+      const row = document.createElement("tr");
 
-        const nameCell = document.createElement('td');
-        nameCell.textContent = item.name;
-        row.appendChild(nameCell);
+      const nameCell = document.createElement("td");
+      nameCell.textContent = item.name;
+      row.appendChild(nameCell);
 
-        const costCell = document.createElement('td');
-        costCell.textContent = `$${item.cost.toFixed(2)}`;
-        row.appendChild(costCell);
+      const costCell = document.createElement("td");
+      costCell.textContent = `$${item.cost.toFixed(2)}`;
+      row.appendChild(costCell);
 
-        const categoryCell = document.createElement('td');
-        categoryCell.textContent = item.category;
-        row.appendChild(categoryCell);
+      const categoryCell = document.createElement("td");
+      categoryCell.textContent = item.category;
+      row.appendChild(categoryCell);
 
-        tableBody.appendChild(row);
+      tableBody.appendChild(row);
     });
   });
-
 })();
 
 /**
- * 
- * @param {JSON} costs 
+ *
+ * @param {JSON} costs
  * @returns {Map} returns a map, where the key is the category name, and the
  *                  value is the array of items in the category
  */
 function byCategory(costs) {
-    var byCategoryMapping = {};
+  var byCategoryMapping = {};
 
-    costs.forEach(c => {
-        var categoryEntries = c.category in byCategoryMapping ? byCategoryMapping[c.category] : []; 
-        categoryEntries.push(c);
-        byCategoryMapping[c.category] = categoryEntries
-    });
-    return byCategoryMapping
+  costs.forEach((c) => {
+    var categoryEntries =
+      c.category in byCategoryMapping ? byCategoryMapping[c.category] : [];
+    categoryEntries.push(c);
+    byCategoryMapping[c.category] = categoryEntries;
+  });
+  return byCategoryMapping;
 }
 
 /**
- * @param {JSON} expenditures 
+ * @param {JSON} expenditures
  * @returns {Number} returns the total for all purchases for a given year
  */
 function totalCosts(expenditures) {
-    return parseFloat(expenditures.filter((item, _a, _b) => item.cost > 0).reduce((total, cost) => total + (cost.cost || 0), 0).toFixed(2));
+  return parseFloat(
+    expenditures
+      .filter((item, _a, _b) => item.cost > 0)
+      .reduce((total, cost) => total + (cost.cost || 0), 0)
+      .toFixed(2)
+  );
 }
 
 /**
@@ -119,5 +123,10 @@ function totalCosts(expenditures) {
  * @returns {Number} returns the total for all revenue for a given year
  */
 function totalRevenue(expenditures) {
-    return parseFloat(expenditures.filter((item, _a, _b) => item.cost < 0).reduce((total, cost) => total + (cost.cost || 0), 0).toFixed(2));
+  return parseFloat(
+    expenditures
+      .filter((item, _a, _b) => item.cost < 0)
+      .reduce((total, cost) => total + (cost.cost || 0), 0)
+      .toFixed(2)
+  );
 }
--- volunteers.js
+++ volunteers.js
@@ -2,172 +2,179 @@
  * Stores the current state and details for a bubble
  */
 class VolunteerBubble {
-    /**
-     * Create the volunteer bubble object
-     *
-     * Given a name, and the max X & Y for the canvas, will generate a
-     * volunteer bubble with a randomized starting point and velocity
-     * 
-     * @param {String} volunteerName 
-     * @param {Number} maxX
-     * @param {Number} maxY
-     */
-    constructor(volunteerName, maxX, maxY) {
+  /**
+   * Create the volunteer bubble object
+   *
+   * Given a name, and the max X & Y for the canvas, will generate a
+   * volunteer bubble with a randomized starting point and velocity
+   *
+   * @param {String} volunteerName
+   * @param {Number} maxX
+   * @param {Number} maxY
+   */
+  constructor(volunteerName, maxX, maxY) {
+    let potentialX = Math.floor(Math.random() * maxX);
+    // now make it not attached to the edge
+    potentialX = Math.max(potentialX, 50);
+    potentialX = Math.min(potentialX, maxX - 50);
 
-        let potentialX = Math.floor(Math.random() * maxX);
-        // now make it not attached to the edge
-        potentialX = Math.max(potentialX, 50);
-        potentialX = Math.min(potentialX, maxX - 50);
+    let potentialY = Math.floor(Math.random() * maxY);
+    // now make it not attached to the edge
+    potentialY = Math.max(potentialY, 50);
+    potentialY = Math.min(potentialY, maxY - 50);
 
-        let potentialY = Math.floor(Math.random() * maxY);
-        // now make it not attached to the edge
-        potentialY = Math.max(potentialY, 50);
-        potentialY = Math.min(potentialY, maxY - 50);
+    this.name = volunteerName;
+    this.x = potentialX;
+    this.y = potentialY;
+    this.velocityX = Math.floor(2 + Math.random() * 4);
+    this.velocityY = Math.floor(2 + Math.random() * 4);
+    this.radius = 60;
+    this.colour = "#fccfa1";
+  }
 
-
-        this.name = volunteerName;
-        this.x = potentialX;
-        this.y = potentialY;
-        this.velocityX = Math.floor(2 + (Math.random() * 4));
-        this.velocityY = Math.floor(2 + (Math.random() * 4));
-        this.radius = 60;
-        this.colour = '#fccfa1';
+  /**
+   * Gets the text to display in the bubble
+   *
+   * @param {Boolean} debug
+   * @returns The inner bubble text
+   */
+  getText(debug) {
+    if (debug) {
+      return `${this.name}\n(${this.x},${this.y})`;
     }
 
-    /**
-     * Gets the text to display in the bubble
-     *
-     * @param {Boolean} debug
-     * @returns The inner bubble text
-     */
-    getText(debug) {
-        if (debug) {
-            return `${this.name}\n(${this.x},${this.y})`
-        }
+    return this.name;
+  }
 
-        return this.name;
+  /**
+   * Checks to see if the x,y coordinates hit the bubble
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @returns Whether the click event happened in the bubble
+   */
+  isHit(x, y) {
+    const radiusWithBuffer = this.radius * 1.5;
+
+    const bubbleTop = Math.max(this.y - radiusWithBuffer, 0);
+    const bubbleBottom = this.y + radiusWithBuffer;
+    const bubbleLeft = Math.max(this.x - radiusWithBuffer);
+    const bubbleRight = this.x + radiusWithBuffer;
+
+    // console.log(`left: ${bubbleLeft}, right: ${bubbleRight}, top: ${bubbleTop}, bottom: ${bubbleBottom}`)
+
+    return (
+      y < bubbleBottom && y > bubbleTop && x > bubbleLeft && x < bubbleRight
+    );
+  }
+
+  /**
+   * Increases the velocity of the bubble
+   */
+  increaseVelocity() {
+    if (this.velocityX > 0) {
+      this.velocityX = this.velocityX + 5;
+    } else {
+      this.velocityX = this.velocityX - 5;
     }
 
-    /**
-     * Checks to see if the x,y coordinates hit the bubble
-     *
-     * @param {Number} x
-     * @param {Number} y
-     * @returns Whether the click event happened in the bubble
-     */
-    isHit(x, y) {
-        const radiusWithBuffer = this.radius * 1.5;
-
-        const bubbleTop = Math.max(this.y - radiusWithBuffer, 0);
-        const bubbleBottom = this.y + radiusWithBuffer;
-        const bubbleLeft = Math.max(this.x - radiusWithBuffer);
-        const bubbleRight = this.x + radiusWithBuffer;
-
-        // console.log(`left: ${bubbleLeft}, right: ${bubbleRight}, top: ${bubbleTop}, bottom: ${bubbleBottom}`)
-
-        return y < bubbleBottom && y > bubbleTop && x > bubbleLeft && x < bubbleRight;
+    if (this.velocityY > 0) {
+      this.velocityY = this.velocityY + 5;
+    } else {
+      this.velocityY = this.velocityY - 5;
     }
-
-    /**
-     * Increases the velocity of the bubble
-     */
-    increaseVelocity() {
-        if (this.velocityX > 0) {
-            this.velocityX = this.velocityX + 5;
-        } else {
-            this.velocityX = this.velocityX - 5;
-        }
-
-        if (this.velocityY > 0) {
-            this.velocityY = this.velocityY + 5;
-        } else {
-            this.velocityY = this.velocityY - 5;
-        }
-    }
-
+  }
 }
 
-(async function() {
+(async function () {
+  let volunteerBubbles = [];
 
-    let volunteerBubbles = [];
+  const debugDiv = document.getElementById("debug-info");
 
-    const debugDiv = document.getElementById('debug-info');
+  let canvas = document.getElementById("volunteer-canvas");
+  console.log(`${canvas.offsetTop}, ${canvas.clientTop}`);
 
-    let canvas = document.getElementById("volunteer-canvas");
-    console.log(`${canvas.offsetTop}, ${canvas.clientTop}`)
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
- 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-     
-    let l = canvas.getContext('2d');
+  let l = canvas.getContext("2d");
 
-    const button_2024 = document.getElementById("2024-volunteers");
-    button_2024.addEventListener("click", function(event) {
-        const volunteers = ["Hope M.", "Xen E.", "Casey H.", "Maseo B.", "Alex D.", "Greg G.", "Matt F.", "Mew", "Anna W.", "Mike G.", "Mike C.", "Erik B.", "Kelsie W.", "Austin B."];
-        volunteerBubbles = volunteers.map((v) => {
-            return new VolunteerBubble(v, innerWidth, innerHeight)
-        });
+  const button_2024 = document.getElementById("2024-volunteers");
+  button_2024.addEventListener("click", function (event) {
+    const volunteers = [
+      "Hope M.",
+      "Xen E.",
+      "Casey H.",
+      "Maseo B.",
+      "Alex D.",
+      "Greg G.",
+      "Matt F.",
+      "Mew",
+      "Anna W.",
+      "Mike G.",
+      "Mike C.",
+      "Erik B.",
+      "Kelsie W.",
+      "Austin B.",
+    ];
+    volunteerBubbles = volunteers.map((v) => {
+      return new VolunteerBubble(v, innerWidth, innerHeight);
     });
-    button_2024.click();
+  });
+  button_2024.click();
 
-    move();
-     
-    // This function will do the animation
-    function move() {
-     
-        // Clear out the screen for the redraw
-        l.clearRect(0, 0, innerWidth, innerHeight);
+  move();
 
-        volunteerBubbles.forEach((b) => drawBubble(l, b));
-        requestAnimationFrame(move);
-    }
+  // This function will do the animation
+  function move() {
+    // Clear out the screen for the redraw
+    l.clearRect(0, 0, innerWidth, innerHeight);
 
-    canvas.addEventListener('click', function(event) {
-        // I am getting the offset here during the click event because for some reason if I get this
-        // earlier on, the canvas offset is significantly higher than it should be. Maybe something with
-        // the DOM loading later than the canvas value is set? Don't know. But this works
-        // let canvasLeft = canvas.offsetLeft + canvas.clientLeft;
-        // let canvasTop = canvas.offsetTop + canvas.clientTop;
-        // console.log(`${canvas.offsetTop}, ${canvas.clientTop}`)
-        // console.log(`Layer: (${event.layerX},${event.layerY}) Page: (${event.pageX},${event.pageY})`)
+    volunteerBubbles.forEach((b) => drawBubble(l, b));
+    requestAnimationFrame(move);
+  }
 
-        let clickX = event.offsetX;
-        let clickY = event.offsetY;
+  canvas.addEventListener("click", function (event) {
+    // I am getting the offset here during the click event because for some reason if I get this
+    // earlier on, the canvas offset is significantly higher than it should be. Maybe something with
+    // the DOM loading later than the canvas value is set? Don't know. But this works
+    // let canvasLeft = canvas.offsetLeft + canvas.clientLeft;
+    // let canvasTop = canvas.offsetTop + canvas.clientTop;
+    // console.log(`${canvas.offsetTop}, ${canvas.clientTop}`)
+    // console.log(`Layer: (${event.layerX},${event.layerY}) Page: (${event.pageX},${event.pageY})`)
 
-        // // ALSO!!!! On my phone (MouseEvent) the offset event layer doesnt need an offset. Unlike the PointerEvent
-        // // seen from the browser
-        // const eventType = event.__proto__.constructor.name;
-        // // This means that if we are receiving a pointer event, we have to subtract the canvas offsets to get the correct
-        // // point in the canvas. If it is not a pointer event, we do not do this...
-        // if (eventType == 'PointerEvent') {
-        //     // clickX -= canvasLeft;
-        //     // clickY -= canvasTop;
-        // }
+    let clickX = event.offsetX;
+    let clickY = event.offsetY;
 
-        // // Firefox uses mouse events. Which has the offsetX and Y values 
-        // if (eventType == 'MouseEvent') {
-        //     clickX = event.offsetX;
-        //     clickY = event.offsetY;
-        // }
+    // // ALSO!!!! On my phone (MouseEvent) the offset event layer doesnt need an offset. Unlike the PointerEvent
+    // // seen from the browser
+    // const eventType = event.__proto__.constructor.name;
+    // // This means that if we are receiving a pointer event, we have to subtract the canvas offsets to get the correct
+    // // point in the canvas. If it is not a pointer event, we do not do this...
+    // if (eventType == 'PointerEvent') {
+    //     // clickX -= canvasLeft;
+    //     // clickY -= canvasTop;
+    // }
 
-        console.log(`clicked (${clickX},${clickY})`)
+    // // Firefox uses mouse events. Which has the offsetX and Y values
+    // if (eventType == 'MouseEvent') {
+    //     clickX = event.offsetX;
+    //     clickY = event.offsetY;
+    // }
 
-        // debugDiv.textContent = `TYPE: ${event.__proto__.constructor.name} | layer: (${event.layerX},${event.layerY}) | offset: (${event.offsetX},${event.offsetY}) | clicked (${clickX},${clickY}) | ${canvasLeft},${canvasTop} | ${canvas.offsetLeft} ^ ${canvas.clientLeft} | ${canvas.offsetTop} ^ ${canvas.clientTop}`
+    console.log(`clicked (${clickX},${clickY})`);
 
-        volunteerBubbles.forEach(function(bubble) {
-            console.log(bubble.getText(true))
-            if (bubble.isHit(clickX, clickY)) {
-                bubble.colour = chooseRandomColour()
-                bubble.increaseVelocity()
-            }
+    // debugDiv.textContent = `TYPE: ${event.__proto__.constructor.name} | layer: (${event.layerX},${event.layerY}) | offset: (${event.offsetX},${event.offsetY}) | clicked (${clickX},${clickY}) | ${canvasLeft},${canvasTop} | ${canvas.offsetLeft} ^ ${canvas.clientLeft} | ${canvas.offsetTop} ^ ${canvas.clientTop}`
 
-        });
-
+    volunteerBubbles.forEach(function (bubble) {
+      console.log(bubble.getText(true));
+      if (bubble.isHit(clickX, clickY)) {
+        bubble.colour = chooseRandomColour();
+        bubble.increaseVelocity();
+      }
     });
-
+  });
 })();
-
 
 /**
  * Generates a random colour based off the hue saturation lightness alpha model
@@ -175,42 +182,39 @@ class VolunteerBubble {
  * @returns Returns a random colour
  */
 function chooseRandomColour() {
-    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+  return "hsla(" + Math.random() * 360 + ", 100%, 50%, 1)";
 }
 
 /**
  * Draws the current volunteer bubble on the HTML canvas
- * 
+ *
  * @param {*} canvas2dContext
- * @param {VolunteerBubble} bubble 
+ * @param {VolunteerBubble} bubble
  */
 function drawBubble(canvas2dContext, bubble) {
-    // Creating a circle
-    canvas2dContext.beginPath();
-    canvas2dContext.strokeStyle = bubble.colour;
-    canvas2dContext.arc(bubble.x, bubble.y, bubble.radius, 0, Math.PI * 2, false);
-    canvas2dContext.stroke();
+  // Creating a circle
+  canvas2dContext.beginPath();
+  canvas2dContext.strokeStyle = bubble.colour;
+  canvas2dContext.arc(bubble.x, bubble.y, bubble.radius, 0, Math.PI * 2, false);
+  canvas2dContext.stroke();
 
-    canvas2dContext.font = '18pt Palatino Linotype';
-    canvas2dContext.fillStyle = bubble.colour;
-    canvas2dContext.textAlign = 'center';
-    canvas2dContext.fillText(bubble.getText(false), bubble.x, bubble.y+3);
-    
-    
-    // Conditions so that the ball bounces
-    // from the edges
-    if (bubble.radius + bubble.x > innerWidth)
-        bubble.velocityX = 0 - bubble.velocityX;
-    
-    if (bubble.x - bubble.radius < 0)
-        bubble.velocityX = 0 - bubble.velocityX;
-    
-    if (bubble.y + bubble.radius > innerHeight)
-        bubble.velocityY = 0 - bubble.velocityY;
-    
-    if (bubble.y - bubble.radius < 0)
-        bubble.velocityY = 0 - bubble.velocityY;
-    
-    bubble.x = bubble.x + bubble.velocityX;
-    bubble.y = bubble.y + bubble.velocityY;
+  canvas2dContext.font = "18pt Palatino Linotype";
+  canvas2dContext.fillStyle = bubble.colour;
+  canvas2dContext.textAlign = "center";
+  canvas2dContext.fillText(bubble.getText(false), bubble.x, bubble.y + 3);
+
+  // Conditions so that the ball bounces
+  // from the edges
+  if (bubble.radius + bubble.x > innerWidth)
+    bubble.velocityX = 0 - bubble.velocityX;
+
+  if (bubble.x - bubble.radius < 0) bubble.velocityX = 0 - bubble.velocityX;
+
+  if (bubble.y + bubble.radius > innerHeight)
+    bubble.velocityY = 0 - bubble.velocityY;
+
+  if (bubble.y - bubble.radius < 0) bubble.velocityY = 0 - bubble.velocityY;
+
+  bubble.x = bubble.x + bubble.velocityX;
+  bubble.y = bubble.y + bubble.velocityY;
 }
